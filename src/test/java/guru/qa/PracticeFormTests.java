c52b73dede6a86be195ead495a3256d68921a0d0
tony <tony-94@bk.ru>
2021-07-03 23:25:52 +0300

    Updating tests cases

--- src/test/java/guru/qa/PracticeFormTests.java
+++ src/test/java/guru/qa/PracticeFormTests.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Configuration.startMaximized;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
     @BeforeAll
@@ -45,20 +45,39 @@ public class PracticeFormTests {
         // сравнение введенных данных
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text("Julia"));
-        $(".table-responsive").shouldHave(text("Fox"));
-        $(".table-responsive").shouldHave(text("julia@qaguru.com"));
-        $(".table-responsive").shouldHave(text("Female"));
-        $(".table-responsive").shouldHave(text("0123456789"));
-        $(".table-responsive").shouldHave(text("04"));
-        $(".table-responsive").shouldHave(text("March,"));
-        $(".table-responsive").shouldHave(text("1990"));
-        $(".table-responsive").shouldHave(text("Biology"));
-        $(".table-responsive").shouldHave(text("Music"));
-        $(".table-responsive").shouldHave(text("fox.jpeg"));
-        $(".table-responsive").shouldHave(text("Street1"));
-        $(".table-responsive").shouldHave(text("NCR"));
-        $(".table-responsive").shouldHave(text("Noida"));
+        //$(".table-responsive").shouldHave(text("Julia"));
+        //$(".table-responsive").shouldHave(text("Fox"));
+        $("tbody").$(byText("Student Name")).parent().shouldHave(text("Julia Fox"));
+
+        //$(".table-responsive").shouldHave(text("julia@qaguru.com"));
+        $("tbody").$(byText("Student Email")).parent().shouldHave(text("julia@qaguru.com"));
+
+        //$(".table-responsive").shouldHave(text("Female"));
+        $("tbody").$(byText("Gender")).parent().shouldHave(text("Female"));
+
+        //$(".table-responsive").shouldHave(text("0123456789"));
+        $("tbody").$(byText("Mobile")).parent().shouldHave(text("0123456789"));
+
+        //$(".table-responsive").shouldHave(text("04"));
+        //$(".table-responsive").shouldHave(text("March,"));
+        //$(".table-responsive").shouldHave(text("1990"));
+        $("tbody").$(byText("Date of Birth")).parent().shouldHave(text("04 March,1990"));
+
+        //$(".table-responsive").shouldHave(text("Biology"));
+        $("tbody").$(byText("Subjects")).parent().shouldHave(text("Biology"));
+
+        //$(".table-responsive").shouldHave(text("Music"));
+        $("tbody").$(byText("Hobbies")).parent().shouldHave(text("Music"));
+
+        //$(".table-responsive").shouldHave(text("fox.jpeg"));
+        $("tbody").$(byText("Picture")).parent().shouldHave(text("fox.jpeg"));
+
+        //$(".table-responsive").shouldHave(text("Street1"));
+        $("tbody").$(byText("Address")).parent().shouldHave(text("Street1"));
+
+        //$(".table-responsive").shouldHave(text("NCR"));
+        //$(".table-responsive").shouldHave(text("Noida"));
+        $("tbody").$(byText("State and City")).parent().shouldHave(text("NCR Noida"));
 
     }
 }
